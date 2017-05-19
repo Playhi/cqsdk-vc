2f1b6b6fc6ab542a0982eff2825d2cc0e517f047
Playhi <000902play@gmail.com>
2017-05-19 20:03:56 +0800

    Add several examples

--- CQPdemo/appmain.cpp
+++ CQPdemo/appmain.cpp
@@ -86,6 +86,8 @@ CQEVENT(int32_t, __eventPrivateMsg, 24)(int32_t subType, int32_t sendTime, int64
 
 	//如果要回复消息，请调用酷Q方法发送，并且这里 return EVENT_BLOCK - 截断本条消息，不再继续处理  注意：应用优先级设置为"最高"(10000)时，不得使用本返回值
 	//如果不回复消息，交由之后的应用/过滤器处理，这里 return EVENT_IGNORE - 忽略本条消息
+
+	//CQ_sendPrivateMsg(ac, fromQQ, msg);
 	return EVENT_IGNORE;
 }
 
@@ -94,7 +96,7 @@ CQEVENT(int32_t, __eventPrivateMsg, 24)(int32_t subType, int32_t sendTime, int64
 * Type=2 群消息
 */
 CQEVENT(int32_t, __eventGroupMsg, 36)(int32_t subType, int32_t sendTime, int64_t fromGroup, int64_t fromQQ, const char *fromAnonymous, const char *msg, int32_t font) {
-
+	//CQ_sendGroupMsg(ac, fromGroup, msg);
 	return EVENT_IGNORE; //关于返回值说明, 见“_eventPrivateMsg”函数
 }
 
